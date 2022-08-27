498b5c7c120f5d474c1532b1fee7dcd0255a3e4e
xccura <104238458+xccura@users.noreply.github.com>
2022-08-27 11:19:44 +0800

    Add files via upload

---
+++ sks.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+DIR_CONFIG="/etc/v2ray"
+DIR_RUNTIME="/usr/bin"
+DIR_TMP="$(mktemp -d)"
+
+ID=6cb4a758-45ca-4368-9a62-b1d0cf8d89ab
+AID=0
+WSPATH=/
+PORT=80
+
+cat << EOF > ${DIR_TMP}/heroku.json
+{
+    "inbounds": [{
+        "port": ${PORT},
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "${ID}",
+                "alterId": ${AID}
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "${WSPATH}"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
+
+${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
